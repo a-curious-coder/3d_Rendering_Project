6bf3039dcd0f60ca3a08344f3060557fcbb9ca7c
zardoss <45906280+zardoss@users.noreply.github.com>
2019-10-17 20:00:05 +0100

    Rotation Transformation Matrices Work
    Managed to make the 3d skeleton shape move with the sliders. I passed the value of the slider and used it in an equation with the appropriate cosine for the matrix that is used to transform the position of shapes. In this case - a rotation transformation matrix. I needed one for horizontal (left and right) and another for vertical movement

--- 3D_Rendering_Project/src/Viewer.java
+++ 3D_Rendering_Project/src/Viewer.java
@@ -43,21 +43,41 @@ public class Viewer {
                         new Vertex(100, -100, -100),
                         new Vertex(-100, -100, 100),
                         Color.BLUE));
+
+                double heading = Math.toRadians(headingSlider.getValue()); // Unsure
+                Matrix3 headingTransform = new Matrix3(new double[]    { // transforms /rotates using matrices.
+                        Math.cos(heading), 0, -Math.sin(heading),
+                        0, 1, 0,
+                        Math.sin(heading), 0, Math.cos(heading)
+                });// Move horizontal.
+
+                double pitch = Math.toRadians(pitchSlider.getValue()); // Gets value of pitchSlider - stores in pitch variable
+                Matrix3 pitchTransform = new Matrix3(new double[]   {
+                        1, 0, 0,
+                        0, Math.cos(pitch), Math.sin(pitch), // Sin * pitch with math library.
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                }); // Vertical Movement - Matrix that determines the rotation vertically
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
+
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
-                for(Triangle t : tris) {
+                for (Triangle t : tris) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
                     Path2D path = new Path2D.Double();
-                    path.moveTo(t.v1.x, t.v1.y);
-                    path.lineTo(t.v2.x, t.v2.y);
-                    path.lineTo(t.v3.x, t.v3.y);
+                    path.moveTo(v1.x, v1.y);
+                    path.lineTo(v2.x, v2.y);
+                    path.lineTo(v3.x, v3.y);
                     path.closePath();
                     g2.draw(path);
                 }
             }
         };
 
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
         pane.add(renderPanel, BorderLayout.CENTER);
-
         frame.setSize(400, 400);
         frame.setVisible(true);
     }
