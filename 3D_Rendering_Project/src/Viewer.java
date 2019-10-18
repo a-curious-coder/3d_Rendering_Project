4c8a801757ad67508592779dc8a3b9d55e489a35
zardoss <45906280+zardoss@users.noreply.github.com>
2019-10-18 09:37:15 +0100

    Rasterization method
    Have written code displaying the triangle shape - revolves around 'Rasterization' via barycentric coords. Can't use hardware rasterization so have to manually code this in.
    
    Barycentric coords  - Have to compute for each pixel that could lie inside the triangles and discard all those outside.

--- 3D_Rendering_Project/src/Viewer.java
+++ 3D_Rendering_Project/src/Viewer.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
 import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+
 public class Viewer {
 
     public static void main(String[] args) {
@@ -21,25 +23,29 @@ public class Viewer {
         JPanel renderPanel = new JPanel() {
             public void paintComponent(Graphics g)  {
                 Graphics2D g2 = (Graphics2D) g;
-                g2.setColor(Color.BLACK);
-                g2.fillRect(0, 0, getWidth(), getHeight());
+                g2.setColor(Color.BLACK); // set color of g2 to black
+                g2.fillRect(0, 0, getWidth(), getHeight()); // Fill rectangle zone from 0,0 to width/height
 
                 // Rendering happens here.
 
                 ArrayList<Triangle> tris = new ArrayList<>();
-                tris.add(new Triangle(new Vertex(100, 100, 100),
+                tris.add(new Triangle(
+                        new Vertex(100, 100, 100),
                         new Vertex(-100, -100, 100),
                         new Vertex(-100, 100, -100),
                         Color.WHITE));
-                tris.add(new Triangle(new Vertex(100, 100, 100),
+                tris.add(new Triangle(
+                        new Vertex(100, 100, 100),
                         new Vertex(-100, -100, 100),
                         new Vertex(100, -100, -100),
                         Color.RED));
-                tris.add(new Triangle(new Vertex(-100, 100, -100),
+                tris.add(new Triangle(
+                        new Vertex(-100, 100, -100),
                         new Vertex(100, -100, -100),
                         new Vertex(100, 100, 100),
                         Color.GREEN));
-                tris.add(new Triangle(new Vertex(-100, 100, -100),
+                tris.add(new Triangle(
+                        new Vertex(-100, 100, -100),
                         new Vertex(100, -100, -100),
                         new Vertex(-100, -100, 100),
                         Color.BLUE));
@@ -61,17 +67,53 @@ public class Viewer {
 
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
+
+
+                BufferedImage img =
+                        new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+
                 for (Triangle t : tris) {
                     Vertex v1 = transform.transform(t.v1);
                     Vertex v2 = transform.transform(t.v2);
                     Vertex v3 = transform.transform(t.v3);
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(v1.x, v1.y);
-                    path.lineTo(v2.x, v2.y);
-                    path.lineTo(v3.x, v3.y);
-                    path.closePath();
-                    g2.draw(path);
+
+                    // since we are not using Graphics2D anymore,
+                    // we have to do translation manually
+                    v1.x += getWidth() / 2;
+                    v1.y += getHeight() / 2;
+                    v2.x += getWidth() / 2;
+                    v2.y += getHeight() / 2;
+                    v3.x += getWidth() / 2;
+                    v3.y += getHeight() / 2;
+
+                    // compute rectangular bounds for triangle
+                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                    int maxX = (int) Math.min(img.getWidth() - 1,
+                            Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() - 1,
+                            Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+                    double triangleArea =
+                            (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+                    for (int y = minY; y <= maxY; y++) {
+                        for (int x = minX; x <= maxX; x++) {
+                            double b1 =
+                                    ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                            double b2 =
+                                    ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                            double b3 =
+                                    ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                                img.setRGB(x, y, t.color.getRGB());
+                            }
+                        }
+                    }
+
                 }
+
+                g2.drawImage(img, -200  , -200, null);
             }
         };
 
