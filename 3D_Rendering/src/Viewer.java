aba02259799c86ec6d53d2c595eeb350760094e7
zardoss <45906280+zardoss@users.noreply.github.com>
2019-10-18 11:11:33 +0100

    Tetrahedron rotation / colour works
    Had to change out the project files as having issues with IDE identifying errors (Think it was due to the directory I appointed it to maybe? I couldn't figure it out so just created a new project file and pasted my code. )
    
    I have finally created a working tetrahedron with all four sides having their own colours thanks to the 'zBuffer' I created. It rotates according to slider movement - very happy so far.

--- 3D_Rendering/src/Viewer.java
+++ 3D_Rendering/src/Viewer.java
@@ -8,6 +8,7 @@ public class Viewer {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+
         Container pane = frame.getContentPane();
         pane.setLayout(new BorderLayout());
 
@@ -28,13 +29,13 @@ public class Viewer {
 
                 // Rendering happens here.
 
-                ArrayList<Triangle> tris = new ArrayList<>();
-                tris.add(new Triangle(
+                ArrayList<Triangle> tris = new ArrayList<>(); // Arraylist to hold each triangle with their attributes
+                tris.add(new Triangle( // First triangle
                         new Vertex(100, 100, 100),
                         new Vertex(-100, -100, 100),
                         new Vertex(-100, 100, -100),
                         Color.WHITE));
-                tris.add(new Triangle(
+                tris.add(new Triangle( // Second triangle... etc.
                         new Vertex(100, 100, 100),
                         new Vertex(-100, -100, 100),
                         new Vertex(100, -100, -100),
@@ -45,17 +46,19 @@ public class Viewer {
                         new Vertex(100, 100, 100),
                         Color.GREEN));
                 tris.add(new Triangle(
-                        new Vertex(-100, 100, -100),
-                        new Vertex(100, -100, -100),
+                        new Vertex(-100, 100, -100), // Vertex is a point to another point. - These are the x, y and z coords.
+                        new Vertex(100, -100, -100), // Vertex starts at 0 then this one will start from the last set points.
                         new Vertex(-100, -100, 100),
                         Color.BLUE));
+                //Builds a tetrahedron - making use of class Triangle to produce these.
 
-                double heading = Math.toRadians(headingSlider.getValue()); // Unsure
-                Matrix3 headingTransform = new Matrix3(new double[]    { // transforms /rotates using matrices.
+
+                double heading = Math.toRadians(headingSlider.getValue()); // Get value of headingSlider - To be paired with rotation angle of shape
+                Matrix3 headingTransform = new Matrix3(new double[]    { // transforms / rotates using correct transformation matrix.
                         Math.cos(heading), 0, -Math.sin(heading),
                         0, 1, 0,
                         Math.sin(heading), 0, Math.cos(heading)
-                });// Move horizontal.
+                });// Moves the tetrahedron horizontally (right n left)
 
                 double pitch = Math.toRadians(pitchSlider.getValue()); // Gets value of pitchSlider - stores in pitch variable
                 Matrix3 pitchTransform = new Matrix3(new double[]   {
@@ -68,12 +71,17 @@ public class Viewer {
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
 
+                // BufferedImage will represent the tetrahedrom, colours determined by ARGB - A being transparency of colour.
+                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
 
-                BufferedImage img =
-                        new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+                // initialize array with extremely far away depths
+                for (int q = 0; q < zBuffer.length; q++) {
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
 
                 for (Triangle t : tris) {
-                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v1 = transform.transform(t.v1); //
                     Vertex v2 = transform.transform(t.v2);
                     Vertex v3 = transform.transform(t.v3);
 
@@ -87,33 +95,37 @@ public class Viewer {
                     v3.y += getHeight() / 2;
 
                     // compute rectangular bounds for triangle
-                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
-                    int maxX = (int) Math.min(img.getWidth() - 1,
-                            Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x)))); // ceil rounds number to closest int greater than result.
+                    int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x)))); // floor rounds the number to closest number less than result.
+
                     int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
-                    int maxY = (int) Math.min(img.getHeight() - 1,
-                            Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
 
-                    double triangleArea =
-                            (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+                    double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x); // One triangle's area?
 
-                    for (int y = minY; y <= maxY; y++) {
+                    for (int y = minY; y <= maxY; y++) { // Not fully sure what's happening here.
                         for (int x = minX; x <= maxX; x++) {
-                            double b1 =
-                                    ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
-                            double b2 =
-                                    ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
-                            double b3 =
-                                    ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+
+                            double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                            double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                            double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+
+
                             if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
-                                img.setRGB(x, y, t.color.getRGB());
+                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z; //
+                                int zIndex = y * img.getWidth() + x;
+                                if (zBuffer[zIndex] < depth) {
+                                    img.setRGB(x, y, t.color.getRGB());
+                                    zBuffer[zIndex] = depth;
+                                }
                             }
                         }
                     }
 
+
                 }
 
-                g2.drawImage(img, -200  , -200, null);
+                g2.drawImage(img, -200, -200, null); // Draws TetraHedron - refers to buffer, pos 0,0
             }
         };
 
@@ -121,6 +133,9 @@ public class Viewer {
         pitchSlider.addChangeListener(e -> renderPanel.repaint());
         pane.add(renderPanel, BorderLayout.CENTER);
         frame.setSize(400, 400);
+        frame.setTitle("3D Render Project");
+        frame.setLocationRelativeTo(null); // Positions window center
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Closes program when X is clicked
         frame.setVisible(true);
     }
 
@@ -169,6 +184,6 @@ class Matrix3   {  // Handles Matrix-Matrix and Vector-Matrix multiplication
                 in.x * values[0] + in.y * values[3] + in.z * values[6],
                 in.x * values[1] + in.y * values[4] + in.z * values[7],
                 in.x * values[2] + in.y * values[5] + in.z * values[8]
-        );
+        ); // Puts all values into a matrix.
     }
 }
